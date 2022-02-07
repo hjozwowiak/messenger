8127d9bf759abeb10d6c8721b8049f2612ac1a18
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2022-02-07 21:02:54 +0100

    [feat]: create users table

---
+++ apps/storage/priv/repo/migrations/20220207200027_create_users.exs
@@ -0,0 +1,13 @@
+defmodule Storage.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :email, :string
+      add :id, :uuid, primary_key: true
+      add :password_hash, :string
+
+      timestamps()
+    end
+  end
+end

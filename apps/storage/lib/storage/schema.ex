e927c31c8e6976a8732637c45e31d0ddf7f37c2c
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2022-02-15 19:16:07 +0100

    [feat]: create macro for using uuid for primary key by default

---
+++ apps/storage/lib/storage/schema.ex
@@ -0,0 +1,9 @@
+defmodule Storage.Schema do
+	defmacro __using__() do
+		quote do
+			use Ecto.Schema
+			@primary_key {:id, :binary_id, autogenerate: true}
+			@foreign_key_type :binary_id
+		end
+	end
+end

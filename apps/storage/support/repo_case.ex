928fcc5595e11ee106c3fb5542dad9352ebae86a
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2022-02-16 20:20:50 +0100

    [feat]: add a helper function for transforming changeset errors

--- apps/storage/support/repo_case.ex
+++ apps/storage/support/repo_case.ex
@@ -19,4 +19,15 @@ defmodule Storage.RepoCase do
 
     :ok
   end
+
+  @doc """
+  A helper that transform changeset errors to a map of messages.
+  """
+  def errors_on(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
+      Enum.reduce(opts, message, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
